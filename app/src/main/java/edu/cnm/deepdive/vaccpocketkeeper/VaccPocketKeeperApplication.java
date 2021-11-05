10700fea783d757d6961fd8ec6a1053bf2e34e27
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-11-05 01:30:20 -0400

    Updated Application class

--- app/src/main/java/edu/cnm/deepdive/vaccpocketkeeper/VaccPocketKeeperApplication.java
+++ app/src/main/java/edu/cnm/deepdive/vaccpocketkeeper/VaccPocketKeeperApplication.java
@@ -3,6 +3,7 @@ package edu.cnm.deepdive.vaccpocketkeeper;
 import android.app.Application;
 import com.facebook.stetho.Stetho;
 import edu.cnm.deepdive.vaccpocketkeeper.service.VaccpocketkeeperDatabase;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Initializes (in the {@link #onCreate()} method) application-level resources. This class
@@ -16,6 +17,12 @@ public class VaccPocketKeeperApplication extends Application {
     super.onCreate();
     Stetho.initializeWithDefaults(this);
     VaccpocketkeeperDatabase.setContext(this);
+    VaccpocketkeeperDatabase
+        .getInstance()
+        .getVaccineDao()
+        .delete() //act of deleting something forces room to create the database
+        .subscribeOn(Schedulers.io())
+        .subscribe(); //actually make this happen now.
   }
 
 }
