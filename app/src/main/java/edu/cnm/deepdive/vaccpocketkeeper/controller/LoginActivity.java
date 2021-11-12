e9db34646df573b4a4ff56b9eaaab197074bfcba
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-11-11 20:59:32 -0500

    Finished GoogleSignInRepository.

---
+++ app/src/main/java/edu/cnm/deepdive/vaccpocketkeeper/controller/LoginActivity.java
@@ -0,0 +1,15 @@
+package edu.cnm.deepdive.vaccpocketkeeper.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.cnm.deepdive.vaccpocketkeeper.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+    //TODO come back to this
+  }
+}
