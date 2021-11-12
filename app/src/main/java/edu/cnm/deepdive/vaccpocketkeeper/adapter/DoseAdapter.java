a4e9c88a924034de749cb52533f7aee61439e7c5
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-11-11 23:28:19 -0500

    Implemented Dose Fragments, Edit Fragments, Adapters, and Layouts.

---
+++ app/src/main/java/edu/cnm/deepdive/vaccpocketkeeper/adapter/DoseAdapter.java
@@ -0,0 +1,77 @@
+package edu.cnm.deepdive.vaccpocketkeeper.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import edu.cnm.deepdive.vaccpocketkeeper.databinding.ItemDoseBinding;
+import edu.cnm.deepdive.vaccpocketkeeper.model.entity.Dose;
+import java.text.DateFormat;
+import java.util.List;
+
+public class DoseAdapter extends RecyclerView.Adapter<DoseAdapter.Holder> {
+  //TODO: change fragment_edit_dose layouts to doctor spinner dropdown and datepicker widgets
+  private final LayoutInflater inflator;
+  private final DateFormat dateFormat;
+  private final List<Dose> doses;
+  private final OnDoseEditHelper onDoseEditHelper;
+  private final OnDoseDeleteHelper onDoseDeleteHelper;
+
+  public DoseAdapter(Context context, List<Dose> doses,
+      OnDoseEditHelper onDoseEditHelper,
+      OnDoseDeleteHelper onDoseDeleteHelper) {
+    inflator = LayoutInflater.from(context);
+    dateFormat = android.text.format.DateFormat.getDateFormat(context);
+    this.onDoseEditHelper = onDoseEditHelper;
+    this.onDoseDeleteHelper = onDoseDeleteHelper;
+    this.doses = doses;
+  }
+
+  @NonNull
+  @Override
+  public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    return new Holder(ItemDoseBinding.inflate(inflator, parent, false));
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull Holder holder, int position) {
+    holder.bind(position);
+  }
+
+  @Override
+  public int getItemCount() {
+    return doses.size();
+  }
+
+  class Holder extends RecyclerView.ViewHolder {
+
+    private final ItemDoseBinding binding;
+
+    private Holder(@NonNull ItemDoseBinding binding) {
+      super(binding.getRoot());
+      this.binding = binding;
+    }
+
+    private void bind(int position) {
+      //Use contents of model object to set contents of binding fields.
+      Dose dose = doses.get(position);
+      binding.doseVaccineName.setText("Doses for Vaccine: " + dose.getVaccineId().getName());//TODO get this from nav argument/database
+      binding.doseDoctor.setText(dose.getDoctorId().getName());//TODO retrieve this from database
+      binding.dateAdministered.setText(String.valueOf(dose.getDateAdministered()));//TODO double check this line doesn't cause the app to crash
+      binding.doseEdit.setOnClickListener(
+          (v) -> onDoseEditHelper.onDoseClick(dose.getId(), v));
+      binding.doseDelete.setOnClickListener(
+          (v) -> onDoseDeleteHelper.onDoseClick(dose, v));
+    }
+  }
+
+  public interface OnDoseEditHelper {
+    void onDoseClick(long id, View view);
+  }
+
+  public interface OnDoseDeleteHelper {
+    void onDoseClick(Dose dose, View view);
+  }
+}
---
+++ app/src/main/java/edu/cnm/deepdive/vaccpocketkeeper/controller/DoseFragment.java
@@ -0,0 +1,110 @@
+package edu.cnm.deepdive.vaccpocketkeeper.controller;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import com.google.android.material.snackbar.Snackbar;
+import edu.cnm.deepdive.vaccpocketkeeper.R;
+import edu.cnm.deepdive.vaccpocketkeeper.adapter.DoseAdapter;
+import edu.cnm.deepdive.vaccpocketkeeper.databinding.FragmentDoseBinding;
+import edu.cnm.deepdive.vaccpocketkeeper.viewmodel.DoseViewModel;
+
+public class DoseFragment extends Fragment {
+
+  private DoseViewModel viewModel;
+  private FragmentDoseBinding binding;
+
+  //TODO: Fix entire file
+  //TODO: adding doses should increase the total number of doses in the vaccine by 1.
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setHasOptionsMenu(true);
+  }
+
+  public View onCreateView(@NonNull LayoutInflater inflater,
+      ViewGroup container, Bundle savedInstanceState) {
+    binding = FragmentDoseBinding.inflate(inflater, container, false);
+    //binding.submit.setOnClickListener((v) ->
+    //    viewModel.submitGuess(binding.guess.getText().toString().trim()));
+    //binding.guess.setFilters(new InputFilter[]{this});
+    //compiler infers that v is a view : (View v)
+    binding.addDose.setOnClickListener(
+        v -> editDose(0,v));
+    return binding.getRoot();
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    //noinspection ConstantConditions
+//    viewModel = new ViewModelProvider(getActivity()).get(DoseViewModel.class);
+//    getLifecycle().addObserver(viewModel);
+//    viewModel.getThrowable().observe(getViewLifecycleOwner(), this::displayError);
+//    viewModel.getDoses().observe(getViewLifecycleOwner(), this::update); //observes a dose
+    viewModel = new ViewModelProvider(this).get(DoseViewModel.class);
+    viewModel
+        .getDoses()
+        .observe(getViewLifecycleOwner(),(doses) -> {
+          DoseAdapter adapter = new DoseAdapter(getContext(), doses, this::editDose,
+              (dose,v) -> viewModel.deleteDose(dose));//TODO: show alert confirming deletion to user (have delete dose method to confirm and say to delete)
+          binding.doses.setAdapter(adapter);
+        });
+  } //when fragment dies, then cleans up
+
+//  @Override
+//  public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+//    super.onCreateOptionsMenu(menu, inflater);
+//    inflater.inflate(R.menu.play_actions, menu);
+//  }
+
+//  @Override
+//  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//    //return true if menu item that we handled, otherwise return false
+//    boolean handled;
+//    if (item.getItemId() == R.id.new_game) {
+//      handled = true;
+//      binding.guess.getText().clear();
+//      viewModel.startGame();
+//    } else {
+//      handled = super.onOptionsItemSelected(item);
+//    }
+//    return handled;
+//  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+    binding = null;
+  }
+
+//  private void update(Dose dose) { //updates the display model
+//    DoseAdapter adapter = new DoseAdapter(getContext(),
+//        dose.getName());// how we get a context in a fragment
+//    binding.dose.setAdapter(adapter);//this adapter can tell us our guesses
+//  }
+
+  private void displayError(Throwable throwable) {
+    if (throwable != null) {
+      Snackbar snackbar = Snackbar.make(binding.getRoot(),
+          getString(R.string.error_message, throwable.getMessage()),
+          Snackbar.LENGTH_INDEFINITE);
+      snackbar.setAction(R.string.error_dismiss,
+          (v) -> snackbar.dismiss());//gets access to livedata from viewmodel - just observing, run contents of bucket once this object changes.
+      snackbar.show();
+    }
+  }
+
+  public void editDose(long id, View view) {
+    Navigation.findNavController(binding.getRoot())
+        .navigate(DoseFragmentDirections.openDose().setDoseId(id));
+  }
+
+}
